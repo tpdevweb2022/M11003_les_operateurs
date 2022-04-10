372de78c2700fc9e12c0d642291e1e359ba5c869
Anthony Merlier <46730440+ideosconsor@users.noreply.github.com>
2022-04-10 22:40:13 +0200

    ex2 ok

---
+++ M11003-1.php
@@ -0,0 +1,23 @@
+<?php
+
+/* Exercice 1 */
+    // Créer une variable avec le chiffre 12 
+    
+    // Lui ajouter 9 via l'opérateur arithmétique correspondant
+
+    // Afficher la variable avec l'opération effectuée
+    
+    
+/* Exercice 2 */
+    // Prenons l'exemple d'une somme d'argent de 450 euros, composées en billets de 5, 10, et 20 euros
+
+    // Nous savons que nous possédons 6 billets de 20 euros et 52 billets de 5 euros, combien restent-ils de billets de 10 euros ?
+    // Poser les opérations nécesaires au calcul ci-dessous
+
+
+/* Exercice 3*/
+    // La rentrée des classes approchent et les élèves de 6e sont au nombre de 155, il faut tous les placer dans des classes de 30 maximum
+
+    // Combien de classes aurons nous au final ? Développer ci-dessous
+
+    // Combien restera t'il d'enfants dans la dernière classe à être composée ? Développer ci-dessous
---
+++ M11003-2.php
@@ -0,0 +1,14 @@
+<?php
+
+/* EXERCICE 1 */
+    // Après avoir déclarer une nouvelle variable ayant pour valeur 27
+
+    // Comparer cette variable en vérifiant si sa valeur est supérieure ou égale à 35 et dumper le résultat
+
+
+/* EXERCICE 2*/
+    // Après avoir défini une variable comme étant égale à 34
+
+    // Déclarer une autre variable ayant pour valeur "34" (au format string)
+
+    // Effectuer ensuite les comparaisons strictes et non strictes de ces deux variables et dumper le résultat
